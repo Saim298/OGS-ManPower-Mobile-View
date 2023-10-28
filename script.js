ffa2c768aa7c93e88a5c03907b453a178e291852
Saim298 <saimwrifeen298@gmail.com>
2023-10-27 21:26:52 -0700

    First Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: 10,
+    grabCursor: true,
+    autoplay:true,
+    loop: true,
+    pagination: {
+      clickable: true,
+    },
+  });
